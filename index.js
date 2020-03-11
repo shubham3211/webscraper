462175174c6208c1fc1328e3093bc0cda98f77f5
shubham <kumarsinghshubham32111@gmail.com>
2020-03-11 13:39:02 +0530

    make it better

--- index.js
+++ index.js
@@ -3,25 +3,32 @@ const app = express();
 const mongoose = require('mongoose');
 const Job = require('./model/Job')
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-type, Accept");
+  next();
+})
+
 mongoose.connect('mongodb://shubham:shubham123@ds018168.mlab.com:18168/nn-outh-test')
   .then(() => {
     app.listen(5000, () => console.log('app running on port 5000'))
   })
-
-function connect() {
-  
-}
-
-app.get('/jobs', (req, res) => {
-  Job.count({}, (err, count) => {
-    console.log('count', count)
+  .catch((err) => {
+    console.log(err);
   })
+
+app.get('/jobs/:id', (req, res) => {
+  let id = req.params.id;
+  let len = parseInt(id-1)*10;
+  console.log(len);
   Job.find({})
+    .skip(len)
+    .limit(10)
     .then((job) => {
       res.send({
         status: true,
         result: job,
-        message: "Job found successfullt"
+        message: "Job found successfully"
       })
     })
     .catch((err) => {
